3175b1d9a7b5f0b67eede66aa2f7e847d19f5e94
DDello32 <danieldroliveira@gmail.com>
2015-06-05 18:14:06 -0300

    Simulacoes longas para energia cinetica sem atrito e gifs

--- PROJETO_Final/parte1.m
+++ PROJETO_Final/parte1.m
@@ -28,55 +28,58 @@ model = 'parte1simulink';
 load_system(model);
 
 
-%% Simulacao 1
-q0 = [0 0 0 0 0 0]; %Posicao inicial
-sim(model);
-sim1 = figure;
-plot(states);
-xlabel('Tempo(s)')
-ylabel('Angulo(rad)')
-title('Resposta de q')
-legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
-saveas(sim1,'sim1.eps','epsc')
-bot.plot(states.Data, 'delay', 5/length(states.Time));
-%% Simulacao 2
-q0 = [0 pi -pi/2 0 0 0]; %Posicao inicial
-sim(model);
-sim2 = figure;
-plot(states);
-xlabel('Tempo(s)')
-ylabel('Angulo(rad)')
-title('Resposta de q')
-legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
-saveas(sim2,'sim2.eps','epsc')
-figure,
-bot.plot(states.Data, 'delay', 5/length(states.Time));
-%% Simulacao 3
-q0 = [0 pi/2 -pi/2 0 0 0]; %Posicao inicial
-sim(model);
-sim3 = figure;
-plot(states);
-xlabel('Tempo(s)')
-ylabel('Angulo(rad)')
-title('Resposta de q')
-legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
-saveas(sim3,'sim3.eps','epsc')
-bot.plot(states.Data, 'delay', 5/length(states.Time));
-%% Simulacao 4
-q0 = [0 pi/2+0.05 -pi/2 0 0 0]; %Posicao inicial
-sim(model);
-sim4 = figure;
-plot(states);
-xlabel('Tempo(s)')
-ylabel('Angulo(rad)')
-title('Resposta de q')
-legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
-saveas(sim4,'sim4.eps','epsc')
-bot.plot(states.Data, 'delay', 5/length(states.Time));
+% %% Simulacao 1
+% q0 = [0 0 0 0 0 0]; %Posicao inicial
+% sim(model);
+% sim1 = figure;
+% plot(states);
+% xlabel('Tempo(s)')
+% ylabel('Angulo(rad)')
+% title('Resposta de q')
+% legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
+% saveas(sim1,'sim1.eps','epsc')
+% figure,
+% bot.plot(states.Data, 'delay', 5/length(states.Time));
+% %% Simulacao 2
+% q0 = [0 pi -pi/2 0 0 0]; %Posicao inicial
+% sim(model);
+% sim2 = figure;
+% plot(states);
+% xlabel('Tempo(s)')
+% ylabel('Angulo(rad)')
+% title('Resposta de q')
+% legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
+% saveas(sim2,'sim2.eps','epsc')
+% figure,
+% bot.plot(states.Data, 'delay', 5/length(states.Time));
+% %% Simulacao 3
+% q0 = [0 pi/2 -pi/2 0 0 0]; %Posicao inicial
+% sim(model);
+% sim3 = figure;
+% plot(states);
+% xlabel('Tempo(s)')
+% ylabel('Angulo(rad)')
+% title('Resposta de q')
+% legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
+% saveas(sim3,'sim3.eps','epsc')
+% figure,
+% bot.plot(states.Data, 'delay', 5/length(states.Time));
+% %% Simulacao 4
+% q0 = [0 pi/2+0.05 -pi/2 0 0 0]; %Posicao inicial
+% sim(model);
+% sim4 = figure;
+% plot(states);
+% xlabel('Tempo(s)')
+% ylabel('Angulo(rad)')
+% title('Resposta de q')
+% legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
+% saveas(sim4,'sim4.eps','epsc')
+% figure,
+% bot.plot(states.Data, 'delay', 5/length(states.Time));
 %% Simulacoes usando o ode45
 %% Simulacao 1
 q0 = [0 0 0 0 0 0]; %Posicao inicial
-[time, qdyn1, qddyn] = bot.fdyn(5, T, q0, zeros(1,6))
+[time, qdyn1, qddyn] = bot.fdyn(5, T, q0, zeros(1,6));
 sim1ode = figure;
 plot(time, qdyn1);
 xlabel('Tempo(s)')
@@ -88,7 +91,7 @@ figure,
 bot.plot(qdyn1, 'delay', 5/length(time))
 %% Simulacao 2
 q0 = [0 pi -pi/2 0 0 0]; %Posicao inicial
-[time, qdyn2, qddyn] = bot.fdyn(5, T, q0, zeros(1,6))
+[time, qdyn2, qddyn] = bot.fdyn(5, T, q0, zeros(1,6));
 sim2ode = figure;
 plot(time, qdyn2);
 xlabel('Tempo(s)')
@@ -100,7 +103,7 @@ figure,
 bot.plot(qdyn2, 'delay', 5/length(time))
 %% Simulacao 3
 q0 = [0 pi/2 -pi/2 0 0 0]; %Posicao inicial
-[time, qdyn3, qddyn] = bot.fdyn(5, T, q0, zeros(1,6))
+[time, qdyn3, qddyn] = bot.fdyn(5, T, q0, zeros(1,6));
 sim3ode = figure;
 plot(time, qdyn3);
 xlabel('Tempo(s)')
@@ -112,7 +115,7 @@ figure,
 bot.plot(qdyn3, 'delay', 5/length(time))
 %% Simulacao 4
 q0 = [0 pi/2+0.05 -pi/2 0 0 0]; %Posicao inicial
-[time, qdyn4, qddyn] = p560.nofriction().fdyn(5, T, q0, zeros(1,6))
+[time, qdyn4, qddyn] = bot.fdyn(5, T, q0, zeros(1,6));
 sim4ode = figure;
 plot(time, qdyn4);
 xlabel('Tempo(s)')
@@ -151,7 +154,7 @@ p560.plot(qdyn, 'delay', 5/length(time))
 
 %% Simulacao 2
 q0 = [0 0.000001 0 0 0 0]; %Posicao inicial
-[time, qdyn, qddyn] = p560.nofriction('all').fdyn(5, T, q0, zeros(1,6));
+[time, qdyn, qddyn] = p560.nofriction('all').fdyn(30, T, q0, zeros(1,6));
 sime2ode = figure;
 plot(time, qdyn);
 xlabel('Tempo(s)')
@@ -171,7 +174,7 @@ ylabel('Energia(J)')
 title('Energia cinetica do sistema')
 saveas(sime2kin,'sime2kin.eps','epsc')
 figure,
-p560.plot(qdyn, 'delay', 5/length(time))
+p560.plot(qdyn, 'delay', 30/length(time))
 
 %% Simulacao 3
 q0 = qs; %Posicao inicial
@@ -201,15 +204,15 @@ p560.plot(qdyn, 'delay', 5/length(time))
 %% Simulacao 1
 q0 = [0 0 0 0 0 0]; %Posicao inicial
 [time, qdyn, qddyn] = bot.fdyn(5, T, q0, zeros(1,6));
-sime1ode = figure;
+sime1odea = figure;
 plot(time, qdyn);
 xlabel('Tempo(s)')
 ylabel('Angulo(rad)')
 title('Resposta de q')
 legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
-saveas(sime1ode,'sime1ode.eps','epsc')
-sime1kin = figure;
-m = p560.inertia(qdyn);
+saveas(sime1odea,'sime1odea.eps','epsc')
+sime1kina = figure;
+m = bot.inertia(qdyn);
 e = zeros(1, length(qdyn));
 for i = 1:length(qdyn)
     e(i) = 1/2*qddyn(i,:)*m(:,:,i)*qddyn(i,:)';
@@ -218,22 +221,22 @@ plot(time, e);
 xlabel('Tempo(s)')
 ylabel('Energia(J)')
 title('Energia cinetica do sistema')
-saveas(sime1kin,'sime1kin.eps','epsc')
+saveas(sime1kina,'sime1kina.eps','epsc')
 figure,
 bot.plot(qdyn, 'delay', 5/length(time))
 
 %% Simulacao 2
 q0 = [0 0.000001 0 0 0 0]; %Posicao inicial
-[time, qdyn, qddyn] = p560.fdyn(5, T, q0, zeros(1,6));
-sime2ode = figure;
+[time, qdyn, qddyn] = bot.fdyn(5, T, q0, zeros(1,6));
+sime2odea = figure;
 plot(time, qdyn);
 xlabel('Tempo(s)')
 ylabel('Angulo(rad)')
 title('Resposta de q')
 legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
-saveas(sime2ode,'sime2ode.eps','epsc')
-sime2kin = figure;
-m = p560.inertia(qdyn);
+saveas(sime2odea,'sime2odea.eps','epsc')
+sime2kina = figure;
+m = bot.inertia(qdyn);
 e = zeros(1, length(qdyn));
 for i = 1:length(qdyn)
     e(i) = 1/2*qddyn(i,:)*m(:,:,i)*qddyn(i,:)';
@@ -242,22 +245,22 @@ plot(time, e);
 xlabel('Tempo(s)')
 ylabel('Energia(J)')
 title('Energia cinetica do sistema')
-saveas(sime2kin,'sime2kin.eps','epsc')
+saveas(sime2kina,'sime2kina.eps','epsc')
 figure,
 bot.plot(qdyn, 'delay', 5/length(time))
 
 %% Simulacao 3
 q0 = qs; %Posicao inicial
-[time, qdyn, qddyn] = p560.fdyn(5, T, q0, zeros(1,6));
-sime3ode = figure;
+[time, qdyn, qddyn] = bot.fdyn(5, T, q0, zeros(1,6));
+sime3odea = figure;
 plot(time, qdyn);
 xlabel('Tempo(s)')
 ylabel('Angulo(rad)')
 title('Resposta de q')
 legend('q_1', 'q_2', 'q_3', 'q_4', 'q_5', 'q_6');
-saveas(sime3ode,'sime3ode.eps','epsc')
-sime3kin = figure;
-m = p560.inertia(qdyn);
+saveas(sime3odea,'sime3odea.eps','epsc')
+sime3kina = figure;
+m = bot.inertia(qdyn);
 e = zeros(1, length(qdyn));
 for i = 1:length(qdyn)
     e(i) = 1/2*qddyn(i,:)*m(:,:,i)*qddyn(i,:)';
@@ -266,7 +269,7 @@ plot(time, e);
 xlabel('Tempo(s)')
 ylabel('Energia(J)')
 title('Energia cinetica do sistema')
-saveas(sime3kin,'sime3kin.eps','epsc')
+saveas(sime3kina,'sime3kina.eps','epsc')
 figure,
 bot.plot(qdyn, 'delay', 5/length(time))
 
